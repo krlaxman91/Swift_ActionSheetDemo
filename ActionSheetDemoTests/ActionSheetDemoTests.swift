117174e867ed8ee10f78bc9f5b8baa9f3e9e1349
kumar laxman jee <kralxman.mec@gmail.com>
2015-05-07 16:28:39 +0530

    Initial Commit

---
+++ ActionSheetDemoTests/ActionSheetDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ActionSheetDemoTests.swift
+//  ActionSheetDemoTests
+//
+//  Created by Laxman on 07/05/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ActionSheetDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
