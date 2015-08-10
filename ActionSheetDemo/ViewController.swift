62b4f36d767a971c0298d0ed720e221c5ffc7e70
kumar laxman jee <kralxman.mec@gmail.com>
2015-08-10 18:56:17 +0530

    Sample Code

--- ActionSheetDemo/ViewController.swift
+++ ActionSheetDemo/ViewController.swift
@@ -21,5 +21,40 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
+    @IBAction func showActionSheet(sender: AnyObject) {
+        // 1
+        let optionMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .ActionSheet)
+        
+        // 2
+        let deleteAction = UIAlertAction(title: "Delete", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            println("File Deleted")
+        })
+        let saveAction = UIAlertAction(title: "Save", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            println("File Saved")
+        })
+        
+        //
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {
+            (alert: UIAlertAction!) -> Void in
+            println("Cancelled")
+        })
+        
+        
+        // 4
+        optionMenu.addAction(deleteAction)
+        optionMenu.addAction(saveAction)
+        optionMenu.addAction(cancelAction)
+        
+        // 5
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+    }
+    
+    
+    
+    
 }
 
